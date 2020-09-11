54f23c59d01713260794075ab7f9aa396242f015
Thomas <thomasroff390@gmail.com>
2020-09-11 17:12:51 +1200

    App rendering notes from notes.js

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+  {
+    key: 1,
+    title: "Lunch",
+    content: "Meeting Steve at Jo's Cafe 12pm Thursday"
+  },
+  {
+    key: 2,
+    title: "Jokes",
+    content: "Q. Why did the programmer leave his job? A. Because he didn't get arrays."
+  }
+]
+
+export default notes;
