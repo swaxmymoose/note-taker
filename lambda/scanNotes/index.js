e079c409f418b73c6ef102c2cd17ba171c6f2a78
Thomas <thomasroff390@gmail.com>
2020-09-17 20:50:42 +1200

    Refactor to async/await and returns http response.

--- lambda/scanNotes/index.js
+++ lambda/scanNotes/index.js
@@ -1,32 +1,30 @@
 const AWS = require("aws-sdk");
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context) => {
     const docClient = new AWS.DynamoDB.DocumentClient();
+    let responseBody = "";
+    let statusCode = 0;
     const params = {
         TableName: "noteTable",
         ProjectionExpression: "title, content",
     }
 
-    console.log("Scanning Notes table.");
-    docClient.scan(params, onScan);
-    
-    function onScan(err, data) {
-        if (err) {
-            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            // print all the notes
-            console.log("Scan succeeded.");
-            data.Items.forEach(function(note) {
-                console.log(note.title, note.content);     
-            });
-            
-            // continue scanning if we have more notes, because
-            // scan can retrieve a maximum of 1MB of data
-            if (typeof data.LastEvaluatedKey != "undefined") {
-                console.log("Scanning for more...");
-                params.ExclusiveStartKey = data.LastEvaluatedKey;
-                docClient.scan(params, onScan);
-            }   
-        }
+    try {
+        const data = await docClient.scan(params).promise();
+        responseBody = JSON.stringify(data.Items);
+        statusCode = 200;
+    } catch (err) {
+        responseBody = "Unable to get notes data";
+        statusCode = 403;
     }
+    
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "myHeader": "test"
+        },
+        body: responseBody
+    }
+    
+    return response;
 }
