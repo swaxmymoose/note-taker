e2ca4f7199bea55b12c92932337f9d46cd0c5fc8
Thomas <thomasroff390@gmail.com>
2020-10-02 12:33:37 +1300

    Note POST method.

--- src/components/App.js
+++ src/components/App.js
@@ -25,19 +25,46 @@ function App() {
     );
   }
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+  
+  /* Add Note - sends post request API */
+  async function addNote(newNote, event) {
+    //setNotes(prevNotes => {
+    //  return [...prevNotes, newNote];
+    // });
+    console.log(newNote);
+    let randId = Math.floor(Math.random() * Math.floor(100000));
+    let randIdString = randId.toString();
+    try {
+        const params = {
+            "id": randIdString,
+            "title": newNote.title,
+            "content": newNote.content
+        }
+        await axios.post('https://u4fqzfwd0c.execute-api.us-east-1.amazonaws.com/prod/note', params);
+        loadNotes();
+    } catch (err) {
+        console.log(err);
+    }
   }
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-	return index !== id;
-      })
-    });   
+// ----- DELETE NOT IMPLEMENTED --------
+  async function deleteNote(id) {
+    alert("Sorry, delete has not been implemented."); /*
+  //   setNotes(prevNotes => {
+  //    return prevNotes.filter((noteItem, index) => {
+  //	return index !== id;
+  //    })
+  //  });
+
+    console.log("Trying to delete note id: " + id);
+
+    try {
+        await axios.delete('https://u4fqzfwd0c.execute-api.us-east-1.amazonaws.com/prod/note/102');
+    } catch (err) {
+        console.log(err);
+    } */
   }
+
 
   return <div>
     <Header/>
